28916f259fc7bf9c67a6d2fc6a48bcd0b21adbf5
Emmanuel Figueroa <superkiko80@hotmail.com>
2023-09-15 20:42:12 -0600

    otra config de database

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -85,10 +85,11 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost:5432/prueba',
-            conn_max_age=600
-        )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))  
+    # 'default': dj_database_url.config(
+    #         default='postgresql://postgres:postgres@localhost:5432/prueba',
+    #         conn_max_age=600
+    #     )
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
     }
