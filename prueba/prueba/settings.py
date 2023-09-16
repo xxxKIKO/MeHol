aa7f700bdc077f898860bc6a3b880b8008017d07
Emmanuel Figueroa <superkiko80@hotmail.com>
2023-09-15 22:45:27 -0600

    activando D y Static

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-        STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# if not DEBUG:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS=[STATIC_DIR,]
 
